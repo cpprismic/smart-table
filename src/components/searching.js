8f00b89606b8cd4dd748415a1282ca3ed625bbce
cpprismic <litvinovkirand@mail.ru>
2026-03-18 16:50:34 +0400

    fixed filtering errors 7

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,12 +3,12 @@ import {rules, createComparison} from "../lib/compare.js";
 export function initSearching(searchField) {
 
     const compare = createComparison(
-        rules.skipEmptyTargetValues,
-        rules.searchMultipleFields(
+        ['skipEmptyTargetValues'],
+        [rules.searchMultipleFields(
             searchField,                 // имя поля в state (например "search")
             ['date', 'customer', 'seller'], // по каким полям искать в данных
             false                        // регистр не учитывать (если библиотека так трактует)
-        )
+        )]
     );
 
     return (data, state, action) => {
